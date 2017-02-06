68e5915d60939c67dbba0ddd7845fcb7037a8496
limelime <limelime@users.noreply.github.com>
2017-02-06 13:27:18 -0500

    Fix path.

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 set -e
 # Description: Deploy
 
-yes | cp -av extract*.sh /usr/loca/bin/
+yes | cp -av extract*.sh /usr/local/bin/
 yes | cp -av extract*.sh /media/master/github/cust-live-deb/scripts/repository/inst-min-con-compressors/extract/
