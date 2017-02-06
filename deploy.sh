ef267fd3601a5405c874a2236a1b668692f901e2
limelime <limelime@users.noreply.github.com>
2017-02-06 12:53:04 -0500

    ADD: for CLD.

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+# Description: Deploy
+
+yes | cp -av extract*.sh /usr/loca/bin/
+yes | cp -av extract*.sh /media/master/github/cust-live-deb/scripts/repository/inst-min-con-compressors/extract/
